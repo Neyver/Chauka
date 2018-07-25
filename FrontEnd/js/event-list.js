e85d42b13120ef3145af83ed68587df35553fa15
SoledadFernandez <soledad.fernandez@fundacion-jala.org>
2018-07-25 19:56:03 -0400

    Commit for merge BLL changes

--- FrontEnd/js/event-list.js
+++ FrontEnd/js/event-list.js
@@ -1,9 +1,10 @@
 const Account = localStorage.getItem('Account');
+const UserEvents = localStorage.getItem('EventsUser');
 console.log(Account);
 
 function fillPage() {
   fillHeader();
-  fillEvents();
+  getEvents();
 }
 
 function fillHeader() {
@@ -14,6 +15,57 @@ function fillHeader() {
   headerAccount.innerHTML = contentHeaderAccount;
 }
 
-function fillEvents() {
+function getEvents() {
+  const account = JSON.parse(Account);
+  const userId = account['Id'];
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+          console.log(this.responseText);
+          let obj = JSON.parse(this.responseText);
+          if(obj.Success===true){
+              localStorage.setItem('EventsUser', JSON.stringify(obj.Data));
+              fillTableEvents();
+          }
+          else {
+              alert("ERROR: \n"+obj.Message);
+          }
 
+      }
+      else {
+          if (this.readyState == 4 && this.status != 200) {
+              if (this.readyState == 4 && this.status == 400) {
+                  let obj = JSON.parse(this.responseText);
+                  alert(obj.Message);
+              }
+              else {
+                  alert("Unexpected error!\n could not be processed.");
+              }
+              console.log(this.responseText);
+              console.log(this.statusText);
+              console.log(this.readyState);
+              console.log(this.getAllResponseHeaders());
+          }
+      }
+  };
+  xhttp.open("GET", "http://localhost:5387/api/Events?userId=" + userId +"", true);
+  xhttp.send();
+}
+
+function fillTableEvents() {
+  if(UserEvents !== null) {
+    const userEvents = JSON.parse(UserEvents);
+    let tableEvents = document.getElementById('table-events-user');
+    let contentTable = "";
+    let events = userEvents['Events'];
+    events.forEach((event) => {
+      contentTable += '<tr>\
+        <td>' + event['NameEvent'] + '</td>\
+        <td>' + event['StartDatetime'] + '</td>\
+        <td>' + event['EndDatetime'] + '</td>\
+        <td> <a href="info-event?Id=' + event['Id'] + '.html"> <i class="material-icons">insert_drive_file</i> </a> </td>\
+      </tr>'
+    });
+    tableEvents.innerHTML = contentTable;
+  }
 }
