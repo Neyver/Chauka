695319739deb7598c8f2288f3ff649abb464596f
Neyver <neyver_365@hotmail.com>
2018-07-25 19:47:19 -0400

    Revert "Bl method get evests"

--- BackEnd/Model/Core/IEventsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Model.Core
-{
-    using System.Collections.Generic;
-
-    public interface IEventsRepository<T> : IRepository<T> where T : IEntity
-    {
-        IEnumerable<T> GetEventsByUserId(int userId);
-    }
-}
