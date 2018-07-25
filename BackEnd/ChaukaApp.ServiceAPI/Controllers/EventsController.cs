571c016d57cb7f56cf7d874a66663e842e64b1be
Administrator <Administrator@TRABAJO>
2018-07-25 15:32:28 -0400

    create service of events

---
+++ BackEnd/ChaukaApp.ServiceAPI/Controllers/EventsController.cs
@@ -0,0 +1,19 @@
+﻿namespace ChaukaApp.ServiceAPI.Controllers
+{
+    using System.Web.Http;
+    using BusinessLogic;
+    using DataAccess;
+    using Model.Core;
+    using Model.Object;
+    using Model.Result;
+
+    public class EventsController : ApiController
+    {
+        // GET api/events
+        /*public IResult<UserEvents> Get(int userId)
+        {
+               
+            return ;
+        }*/
+    }
+}
