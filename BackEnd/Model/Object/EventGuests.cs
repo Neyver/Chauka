340837bda8853c66e97355430d811ec367aa9903
Sandy Garcia <sfgcusi@gmail.com>
2018-07-27 10:22:19 -0400

    Model Guest EventGuests and GuestController

---
+++ BackEnd/Model/Object/EventGuests.cs
@@ -0,0 +1,11 @@
+﻿namespace Model.Object
+{
+    using System.Collections.Generic;
+
+    public class EventGuests
+    {
+        public int UserId { get; set; }
+
+        public IEnumerable<Guest> Guest { get; set; }
+    }
+}
---
+++ BackEnd/Model/Object/Guest.cs
@@ -0,0 +1,17 @@
+﻿namespace Model.Object
+{
+    using System.ComponentModel.DataAnnotations;
+    using Model.Core;
+
+    public class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int EventId { get; set; }
+    }
+}
