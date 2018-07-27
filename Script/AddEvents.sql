14304e08fe6c71ff79d1e0f21a9b131ee28a7fb6
SoledadFernandez <soledad.fernandez@fundacion-jala.org>
2018-07-26 21:02:08 -0400

    Scripts added

---
+++ Script/AddEvents.sql
@@ -0,0 +1,57 @@
+USE [ChaukaDb]
+GO
+
+INSERT INTO [dbo].[Events]
+           ([NameEvent]
+           ,[StartDatetime]
+           ,[EndDatetime]
+           ,[UserId]
+           ,[Latitude]
+           ,[Longitude])
+     VALUES
+           ('Event1', '20120618 10:34:09 AM', '20120618 10:34:09 PM', 1, -17.3627128, -66.1915697)
+GO
+
+INSERT INTO [dbo].[Events]
+           ([NameEvent]
+           ,[StartDatetime]
+           ,[EndDatetime]
+           ,[UserId]
+           ,[Latitude]
+           ,[Longitude])
+     VALUES
+           ('Event2', '20120618 10:34:09 AM', NULL, 1, -17.3627128, -66.1915697)
+GO
+
+INSERT INTO [dbo].[Events]
+           ([NameEvent]
+           ,[StartDatetime]
+           ,[EndDatetime]
+           ,[UserId]
+           ,[Latitude]
+           ,[Longitude])
+     VALUES
+           ('Event3', '20120618 10:34:09 AM', '20120618 10:34:09 PM', 1, -17.3627128, -66.1915697)
+GO
+
+INSERT INTO [dbo].[Events]
+           ([NameEvent]
+           ,[StartDatetime]
+           ,[EndDatetime]
+           ,[UserId]
+           ,[Latitude]
+           ,[Longitude])
+     VALUES
+           ('Event4', '20120618 10:34:09 AM', '20120618 10:34:09 PM', 1, -17.3627128, -66.1915697)
+GO
+
+INSERT INTO [dbo].[Events]
+           ([NameEvent]
+           ,[StartDatetime]
+           ,[EndDatetime]
+           ,[UserId]
+           ,[Latitude]
+           ,[Longitude])
+     VALUES
+           ('Event5', '20120618 10:34:09 AM', '20120618 10:34:09 PM', 2, -17.3627128, -66.1915697)
+GO
