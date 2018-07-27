6e9c05d41caa808fad99ef2c9bee0094600021bb
Jhannet <jhannet.jancko.camacho@gmail.com>
2018-07-27 14:13:01 -0400

    Create function GetEvent

--- BackEnd/ChaukaApp.ServiceAPI/Controllers/EventsController.cs
+++ BackEnd/ChaukaApp.ServiceAPI/Controllers/EventsController.cs
@@ -27,6 +27,26 @@
             return resultEvent;
         }
 
+        // GET api/events?userId=1&userEvent=1
+        [HttpGet]
+        public IResult<Event> GetEvent(int eventId)
+        {
+            IResult<Event> resultEvent = new ResultEntity<Event>();
+            try
+            {
+                IEventHost eventHost = new EventHost();
+
+                //resultEvent = eventHost.GetEvent(userId, eventId);
+            }
+            catch (Exception)
+            {
+                resultEvent.Success = false;
+                resultEvent.Message = "The service could not respond to your request";
+            }
+
+            return resultEvent;
+        }
+
         public ResultSimplified Post(Event newEvent)
         {
             ResultSimplified resultRegister;
